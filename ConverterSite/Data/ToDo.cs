786f877013a464199df56d030de8e9f93d0b6691
skoleoplaeringscenter <58067326+rasmuswork@users.noreply.github.com>
2023-08-21 13:19:50 +0200

    TaskWebsiteInitcommit

---
+++ ConverterSite/Data/ToDo.cs
@@ -0,0 +1,10 @@
+﻿namespace ConverterSite.Data;
+
+public class Todo
+{
+    public int Id;
+    public string? Title { get; set; }
+    public bool IsDone { get; set; } //property
+}
+
+//check date and return day schedule, the current timeblock needs to be highlighted
