6e6fcdf694203dab5f9211bb9ee991ca0e4ce2a0
skoleoplaeringscenter <kuraihjorth@gmail.com>
2023-08-28 13:04:40 +0200

    Updated tables

--- ConverterSite/Data/ToDo.cs
+++ ConverterSite/Data/ToDo.cs
@@ -4,7 +4,7 @@ public class Todo
 {
     public int Id;
     public string? Title { get; set; }
-    public bool IsDone { get; set; } //property
+    
 }
 
 //check date and return day schedule, the current timeblock needs to be highlighted
